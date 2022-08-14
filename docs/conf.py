363127977ea5eb969433444f40b73ea1f21735a4
marple-git <marple.te@yandex.ru>
2022-08-14 13:46:45 +0300

    Updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,18 +1,8 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'pyonlinesim'
 copyright = '2022, Marple'
 author = 'Marple'
 release = '1.0.1'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     'sphinx.ext.autodoc',
@@ -26,32 +16,9 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_theme = 'furo'
 html_logo = '_static/logo_black.svg'
 html_static_path = ['_static']
 
 source_suffix = '.rst'
 master_doc = 'index'
-
-# If true, '()' will be appended to :func: etc. cross-reference text.
-add_function_parentheses = True
-
-latex_documents = [
-    (master_doc, f'{project}.tex', f'{project} Documentation', author, 'manual'),
-]
-
-man_pages = [(master_doc, project, f'{project} Documentation', [author], 1)]
-
-texinfo_documents = [
-    (
-        master_doc,
-        project,
-        f'{project} Documentation',
-        author,
-        project,
-        'Asynchronous wrapper to interact with onlinesim.ru API',
-    ),
-]
