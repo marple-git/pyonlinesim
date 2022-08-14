a72c103dc6c94c5fa6574681d259cb25051e0110
marple-git <marple.te@yandex.ru>
2022-08-14 20:35:45 +0300

    updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,3 +24,26 @@ html_static_path = ['_static']
 
 source_suffix = '.rst'
 master_doc = 'index'
+
+rst_prolog = """
+.. role:: pycode(code)
+   :language: python3
+"""
+
+latex_documents = [
+    (master_doc, f'{project}.tex', f'{project} Documentation', author, 'manual'),
+]
+
+man_pages = [(master_doc, project, f'{project} Documentation', [author], 1)]
+
+texinfo_documents = [
+    (
+        master_doc,
+        project,
+        f'{project} Documentation',
+        author,
+        project,
+        'Modern and fully asynchronous framework for Telegram Bot API',
+        'Miscellaneous',
+    ),
+]
