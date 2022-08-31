118ebe1d37b8f0fa13bb55632fc767ae09c2715f
marple-git <marple.te@yandex.ru>
2022-08-31 07:37:05 +0300

    🔥 Added OrderStatus for OnlineSMS Wrapper

--- pyonlinesim/types/sms/state.py
+++ pyonlinesim/types/sms/state.py
@@ -1,10 +1,19 @@
+from enum import Enum
 from typing import List, Optional
 
 from pydantic import BaseModel, Field
 
 
+class OrderStatus(str, Enum):
+    WAITING_FOR_NUMBER = 'TZ_INPOOL'
+    WAITING_FOR_ANSWER = 'TZ_NUM_WAIT'
+    RECEIVED_ANSWER = 'TZ_NUM_ANSWER'
+    ANSWER_NOT_RECEIVED = 'TZ_OVER_EMPTY'
+    ORDER_FINISHED = 'TZ_OVER_OK'
+
+
 class OrderState(BaseModel):
-    response: str
+    response: OrderStatus
     operation_id: int = Field(..., alias='tzid')
     service: str
     number: str
